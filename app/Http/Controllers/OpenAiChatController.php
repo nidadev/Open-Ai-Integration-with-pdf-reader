03b7c137c4935568564f757b39b163d6d0be20bb
nida <nidazeeshan15@gmail.com>
2024-04-24 22:43:47 +0500

    Update OpenAiChatController.php

--- app/Http/Controllers/OpenAiChatController.php
+++ app/Http/Controllers/OpenAiChatController.php
@@ -12,5 +12,25 @@ class OpenAiChatController extends Controller
     public function openAiChat() :JsonResponse
     {
         $search = "what is google";
+        $data = Http::WithHeaders([
+            "Content-Type" => 'application/json',
+            "Authorization" => 'Bearer'.env('OPEN_API_KEY'),
+        ])->post('https://api.openai.com/v1/completions',[
+            'model' => 'gpt-3.5-turbo-instruct',
+            'messages' => [
+            [
+                'role' => 'user',
+                'content' => $search
+            ]
+                ],
+                'temperature' => 0.5,
+                'max_tokens'  => 200,
+                'top_p' => 1.0,
+                'frequency_penalty' => 0.52,
+                'presence_penalty' => 0.5,
+                'stop' => ["11."]
+
+        ])
+        ;
     }
 }
