684e6bf303f982b056767e51260057f0448d12ac
nida <nidazeeshan15@gmail.com>
2024-04-24 21:17:26 +0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PDFExtractController;
+use App\Http\Controllers\ProgressBarController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+;
+Route::get('pdf',[PDFExtractController::class,'extractPdf']);
+Route::get('progress',[ProgressBarController::class,'index']);
+Route::post('upload-pdf',[ProgressBarController::class,'uploadToSrv']);
