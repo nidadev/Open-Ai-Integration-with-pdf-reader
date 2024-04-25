964a2cf9f23af7ef4a1570f72cc4736d4fdcb959
nida <nidazeeshan15@gmail.com>
2024-04-25 09:37:56 +0500

    upd

--- app/Http/Controllers/OpenAiChatController.php
+++ app/Http/Controllers/OpenAiChatController.php
@@ -5,25 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;  //for calling api
 use Illuminate\Http\JsonResponse;
+use LucianoTonet\Groq;
 
 class OpenAiChatController extends Controller
 {
     //
     public function openApiChat() :JsonResponse
     {
-        $search = "what is google";
+        $search = "what is pm";
         $data = Http::withHeaders([
             'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
+            'Authorization' => 'Bearer '.env('GROQ_API_KEY'),
             
-        ])->post('https://api.openai.com/v1/completions',[
-            'model' => 'gpt-3.5-turbo-instruct',
+        ])->post('https://api.groq.com/openai/v1/chat/completions',[
             'messages' => [
             [
                 'role' => 'user',
                 'content' => $search
             ]
                 ],
+                'model' => 'mixtral-8x7b-32768',
+
                 'temperature' => 0.5,
                 'max_tokens'  => 200,
                 'top_p' => 1.0,
@@ -33,6 +35,19 @@ class OpenAiChatController extends Controller
                 ])->json();
                 //dd($data);
 
-        return response()->json($data, 200,array(),JSON_PRETTY_PRINT);
+        return response()->json($data["choices"][0]['message'], 200,array(),JSON_PRETTY_PRINT);
+        /*$groq = new Groq(getenv('GROQ_API_KEY'));
+
+$chatCompletion = $groq->chat()->completions()->create([
+  'model'    => 'mixtral-8x7b-32768',
+  'messages' => [
+    [
+      'role'    => 'user',
+      'content' => 'Explain the importance of low latency LLMs'
+    ],
+  ]
+]);
+
+echo $chatCompletion['choices'][0]['message']['content'];*/
     }
 }
