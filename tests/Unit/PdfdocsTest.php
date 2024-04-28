52d81b085b58f14815e6141f4509bade9e36de2c
nida <nidazeeshan15@gmail.com>
2024-04-28 21:23:07 +0500

    upd

--- tests/Unit/PdfdocsTest.php
+++ tests/Unit/PdfdocsTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Unit;
 
 //use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Pdfdoc;
 use DB;
 class PdfdocsTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      */
@@ -21,8 +23,9 @@ class PdfdocsTest extends TestCase
     public function test_check_if_pdfdocs_data_fetched_with_id()
     {
         $pdfdata = Pdfdoc::factory(10)->create();
-        $response = DB::table('pdfdocs')->first();
-        $this->assertEquals(1,$response->id);
+        $response = DB::table('pdfdocs')->get()->toArray();
+        $this->assertArrayHasKey(9,$response);
+        $this->assertCount(10,$response);
         //dd($response);
     }
 }
--- tests/Unit/TextDataTest.php
+++ tests/Unit/TextDataTest.php
@@ -4,11 +4,13 @@ namespace Tests\Unit;
 
 //use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\TextData;
 use DB;
 
 class TextDataTest extends TestCase
 {
+     use RefreshDatabase;
     /**
      * A basic unit test example.
      */
@@ -22,8 +24,10 @@ class TextDataTest extends TestCase
     public function test_check_if_text_data_fetched_with_id()
     {
        TextData::factory(10)->create();
-        $response = DB::table('text_data')->first();
-        $this->assertEquals(1,$response->id);
+        $response = DB::table('text_data')->get()->toArray();
+        //$this->assertEquals(1,$response->id);
+        $this->assertArrayHasKey(9,$response);
+        $this->assertCount(10,$response);
         //dd($response);
     }
 }
